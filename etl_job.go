1034572df8431f32518a2599cb5c378fd1d415d1
wind-tangkai <wind.tangkai@gmail.com>
2022-05-05 17:03:40 +0800

    add FunctionConfig.RoleArn for fc trigger

--- etl_job.go
+++ etl_job.go
@@ -56,6 +56,7 @@ type FunctionConfig struct {
 	RegionName       string `json:"regionName"`
 	ServiceName      string `json:"serviceName"`
 	FunctionName     string `json:"functionName"`
+	RoleArn          string `json:"roleArn"`
 }
 
 type JobLogConfig struct {

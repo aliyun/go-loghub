57fa2d77b3958afb2d1a23f00a4e7dcfec4113e0
evanljp <evanljp@outlook.com>
2023-07-27 15:42:23 +0800

    enhance post bytes

--- client_store.go
+++ client_store.go
@@ -103,6 +103,15 @@ func (c *Client) PostLogStoreLogs(project, logstore string, lg *LogGroup, hashKe
 	return ls.PostLogStoreLogs(lg, hashKey)
 }
 
+// PostRawLogWithCompressType put raw log data to log service, no marshal
+func (c *Client) PostRawLogWithCompressType(project, logstore string, rawLogData []byte, compressType int, hashKey *string) (err error) {
+	ls := convertLogstore(c, project, logstore)
+	if err := ls.SetPutLogCompressType(compressType); err != nil {
+		return err
+	}
+	return ls.PostRawLogs(rawLogData, hashKey)
+}
+
 // PutLogsWithCompressType put logs into logstore with specific compress type.
 // The callers should transform user logs into LogGroup.
 func (c *Client) PutLogsWithCompressType(project, logstore string, lg *LogGroup, compressType int) (err error) {
@@ -522,3 +531,11 @@ func (c *Client) UpdateSubStoreTTL(project, logstore string, ttl int) (err error
 	}
 	return
 }
+
+//func (c *Client) PutRawLogs(project, logstore string, lg []byte, compressType int) (err error) {
+//
+//}
+//
+//func (c *Client) PosLogStoreRawLogs(project, logstore string, lg []byte, compressType int, hashKey *string) {
+//
+//}

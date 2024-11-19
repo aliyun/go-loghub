031a8386c6f7e203923deec882df46129fe2c158
crimson <1291463831@qq.com>
2024-11-19 12:00:34 +0800

    feat: add simple metrics for consumer

--- consumer/config.go
+++ consumer/config.go
@@ -49,6 +49,7 @@ type LogHubConfig struct {
 	//:param AutoCommitIntervalInSec: default auto commit interval, default is 30
 	//:param AuthVersion: signature algorithm version, default is sls.AuthV1
 	//:param Region: region of sls endpoint, eg. cn-hangzhou, region must be set if AuthVersion is sls.AuthV4
+	//:param DisableRuntimeMetrics: disable runtime metrics, runtime metrics prints to local log.
 	Endpoint                  string
 	AccessKeyID               string
 	AccessKeySecret           string
@@ -78,6 +79,7 @@ type LogHubConfig struct {
 	AutoCommitIntervalInMS    int64
 	AuthVersion               sls.AuthVersionType
 	Region                    string
+	DisableRuntimeMetrics     bool
 }
 
 const (

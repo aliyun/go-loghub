c7297df4e6e2f59172134e4c0c7cc080b2cc82e4
crimson <1291463831@qq.com>
2025-01-10 23:35:46 +0800

    fix concurrent

--- producer/monitor.go
+++ producer/monitor.go
@@ -1,11 +1,14 @@
 package producer
 
 import (
+	"fmt"
+	"sync"
 	"sync/atomic"
 	"time"
 
 	"github.com/aliyun/aliyun-log-go-sdk/internal"
 	"github.com/go-kit/kit/log"
+	uberatomic "go.uber.org/atomic"
 )
 
 type ProducerMetrics struct {
@@ -19,8 +22,28 @@ type ProducerMetrics struct {
 	waitMemoryFailCount atomic.Int64
 
 	// 临时采样
-	slowFuncs      map[string]int32
-	superSlowFuncs map[string]int32
+	slowFuncs      IntMap
+	superSlowFuncs IntMap
+}
+
+type IntMap struct {
+	m sync.Map
+}
+
+func (im *IntMap) Increment(key string) {
+	v, _ := im.m.LoadOrStore(key, uberatomic.NewInt32(0))
+	v.(*uberatomic.Int32).Inc()
+}
+
+func (im *IntMap) String() string {
+	s := "{"
+	im.m.Range(func(key, value interface{}) bool {
+		v := value.(*uberatomic.Int32)
+		s += fmt.Sprintf("%s: %d,", key.(string), v.Load())
+		return true
+	})
+	s += "}"
+	return s
 }
 
 type ProducerMonitor struct {
@@ -29,10 +52,7 @@ type ProducerMonitor struct {
 
 func newProducerMonitor() *ProducerMonitor {
 	m := &ProducerMonitor{}
-	m.metrics.Store(&ProducerMetrics{
-		slowFuncs:      make(map[string]int32),
-		superSlowFuncs: make(map[string]int32),
-	})
+	m.metrics.Store(&ProducerMetrics{})
 	return m
 }
 
@@ -67,10 +87,7 @@ func (m *ProducerMonitor) incWaitMemoryFail() {
 func (m *ProducerMonitor) getAndResetMetrics() *ProducerMetrics {
 	// we dont need cmp and swap, only one thread would call m.metrics.Store
 	old := m.metrics.Load().(*ProducerMetrics)
-	m.metrics.Store(&ProducerMetrics{
-		slowFuncs:      make(map[string]int32),
-		superSlowFuncs: make(map[string]int32),
-	})
+	m.metrics.Store(&ProducerMetrics{})
 	return old
 }
 
@@ -78,9 +95,9 @@ func (m *ProducerMonitor) recordIfSlow(begin time.Time, name string) {
 	elapsed := time.Since(begin)
 	if elapsed > 1*time.Millisecond {
 		metrics := m.metrics.Load().(*ProducerMetrics)
-		metrics.slowFuncs[name]++
+		metrics.slowFuncs.Increment(name)
 		if elapsed > 10*time.Millisecond {
-			metrics.superSlowFuncs[name]++
+			metrics.superSlowFuncs.Increment(name)
 		}
 	}
 }
@@ -96,17 +113,8 @@ func (m *ProducerMonitor) reportThread(reportInterval time.Duration, logger log.
 			"onFail", metrics.onFail.String(),
 			"waitMemory", metrics.waitMemory.String(),
 			"waitMemoryFailCount", metrics.waitMemoryFailCount.Load(),
-			"slowFuncs", toString(metrics.slowFuncs),
-			"superSlowFuncs", toString(metrics.superSlowFuncs),
+			"slowFuncs", metrics.slowFuncs.String(),
+			"superSlowFuncs", metrics.superSlowFuncs.String(),
 		)
 	}
-}
-
-func toString(m map[string]int32) string {
-	s := "{"
-	for k, v := range m {
-		s += k + ":" + string(v) + ","
-	}
-	s += "}"
-	return s
 }
